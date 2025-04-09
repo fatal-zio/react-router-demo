0a66ac5d73ebcd559288ccb9c579dff78f323510
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-04-09 14:51:53 -0500

    Added menu bar and basic styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
-const router = createBrowserRouter(
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> }
-);
+const router = createBrowserRouter({
+  path: '/',
+  element: <RootLayout />,
+  children: [
+    { path: '/', element: <HomePage /> },
+    { path: '/products', element: <ProductsPage /> },
+  ],
+});
 
 function App() {
   return <RouterProvider router={router} />;
